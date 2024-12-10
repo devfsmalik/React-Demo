13b52388200f85dd21de94ea5898c54431a71b57
usama khan <usamakhan049@gmail.com>
2024-12-10 11:48:03 +0500

    add env support

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import { Alert, Button, DarkThemeToggle, useThemeMode } from "flowbite-react";
 
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+
 function App() {
   const { mode, toggleMode } = useThemeMode();
   return (
     <div className="bg-primar">
       <DarkThemeToggle />
+      <div>{supabaseUrl}</div>
       <Button color="primary" onClick={() => toggleMode()}>{mode}</Button>
       <Alert color="failure">
         <span className="font-medium text-primary">Info alert!</span> Change a
